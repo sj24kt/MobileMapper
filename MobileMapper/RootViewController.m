14b31c241557afa6d8ab0a2006a130a788aac74c
Sherrie Jones <sj24kt@comcast.net>
2015-03-24 07:52:24 -0600

    initial commit

---
+++ MobileMapper/RootViewController.m
@@ -0,0 +1,24 @@
+//
+//  RootViewController.m
+//  MobileMapper
+//
+//  Created by Sherrie Jones on 3/24/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+
+@end
